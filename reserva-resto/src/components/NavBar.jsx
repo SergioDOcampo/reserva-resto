ccac542eec6ef8ee470aa82454f23c4eaaec5c2e
Aris13lo <Aris13lo@gmail.com>
2023-10-19 19:37:41 -0300

    Actualizacion de archivos principales

---
+++ reserva-resto/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <>
+            <nav class="navbar bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <h1>NavBar</h1>
+                    </a>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar;
